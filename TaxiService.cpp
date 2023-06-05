95fb72f2429e336bade1ea33a527798cc8be8415
Yova <Yova@YOVA-LENOVO>
2023-06-05 16:57:07 +0300

    Add writing to binary file functionality to class TaxiService (untested)

--- TaxiService.cpp
+++ TaxiService.cpp
@@ -124,3 +124,37 @@ int TaxiService::finishOrder(unsigned orderID)
 	this->orders[orderID].finish();
 	this->drivers[currentDriverIndex].changeAddress(this->orders[orderID].getTo());
 }
+
+std::ofstream& TaxiService::writeTaxiService(std::ofstream& output) const
+{
+	//write all the clients - first write how many there currently are
+	int numberOfClients = this->clients.getSize();
+	output.write((const char*)&numberOfClients, sizeof(numberOfClients));
+	for (int i = 0; i < numberOfClients; i++)
+	{
+		this->clients[i].writeUser(output);
+	}
+
+	//same process for the drivers
+	int numberOfDrivers = this->drivers.getSize();
+	output.write((const char*)&numberOfDrivers, sizeof(numberOfDrivers));
+	for (int i = 0; i < numberOfDrivers; i++)
+	{
+		this->drivers[i].writeUser(output);
+	}
+
+	//current driver index and current client index will not be recorded, as they are dependent on the
+	//current use session of the programme and do not affect the taxi service records
+
+	int numberOfOrders = this->orders.getSize();
+	output.write((const char*)&numberOfOrders, sizeof(numberOfOrders));
+	for (int i = 0; i < numberOfOrders; i++)
+	{
+		this->orders[i].writeOrder(output);
+	}
+
+	double tempMoney = this->totalProfit;
+	output.write((const char*)&tempMoney, sizeof(tempMoney));
+
+	return output;
+}
--- TaxiService.h
+++ TaxiService.h
@@ -4,11 +4,13 @@
 #include "Client.h"
 #include "Driver.h"
 #include "Order.h"
+
+#include <fstream>
 class TaxiService
 {
 private:
-	MyVector<Client> clients; //User?
-	MyVector<Driver> drivers; //User?
+	MyVector<Client> clients; 
+	MyVector<Driver> drivers;
 	unsigned currentClientIndex = INVALID_INDEX; //when a client logs in, this is set to the index of this user in the vector
 	//it is then used as a sort of client id within the system
 	unsigned currentDriverIndex = INVALID_INDEX; //same thing, but with the drivers
@@ -16,6 +18,8 @@ private:
 	//there would need to be constant checking whether the user is a client or not
 
 	MyVector<Order> orders;
+
+	double totalProfit = 0; //records all profits collected by the drivers
 
 public:
 	//void run();
@@ -32,10 +36,13 @@ public:
 	int pay(unsigned orderID, double amount);
 
 	//driver functions
+	int acceptOrder(unsigned orderID);
 	int acceptPayment(unsigned orderID, double amount);
 	int declineOrder(unsigned orderID);
 	int finishOrder(unsigned orderID);
 
+
+	std::ofstream& writeTaxiService(std::ofstream& output) const;
 
 
 
